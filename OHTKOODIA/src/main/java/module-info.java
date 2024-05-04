fcf24230e7d061b93319707ab85c53fa31313ca3
35844 <marjussi@uef.fi>
2024-05-05 00:02:33 +0300

    tietokannan yhdistystä

--- OHTKOODIA/src/main/java/module-info.java
+++ OHTKOODIA/src/main/java/module-info.java
@@ -2,6 +2,7 @@ module org.example.ohtkoodia {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
+    requires mysql.connector.java;
 
 
     opens org.example.ohtkoodia to javafx.fxml;
--- OHTKOODIA/src/main/java/org/example/ohtkoodia/TietokannanYhdistaminen.java
+++ OHTKOODIA/src/main/java/org/example/ohtkoodia/TietokannanYhdistaminen.java
@@ -2,6 +2,34 @@ package org.example.ohtkoodia;
 import java.sql.*;
 public class TietokannanYhdistaminen {
     public static void main(String[] args) {
+        //Make: en ole varma onkohan localhost nyt 3306 vai 3307
+        String url = "jdbc:mysql://localhost:3306/vn";
+
+        String username = "root";
+        String password = "";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            Connection connection = DriverManager.getConnection(url, username, password);
+
+            Statement statement = connection.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("select * from mokki");
+
+            //datatyyppi + kolumni-indeksi haettavassa taulukossa
+            while (resultSet.next()){
+                System.out.println(resultSet.getInt(1)+resultSet.getString(2));
+            }
+
+            connection.close();
+
+        } catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+        /*Make: Jätin tähän vielä piian tekemän koodin varmuuden vuoksi!
+
         try {
             // Yhdistetään tietokantaan
             String url = "jdbc:mysql://localhost:3306/vn";
@@ -24,6 +52,6 @@ public class TietokannanYhdistaminen {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-    }
+    }*/
 }
 
