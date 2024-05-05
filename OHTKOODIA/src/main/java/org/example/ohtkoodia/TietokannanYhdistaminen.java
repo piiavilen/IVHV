d81455e797e35830d4e0717026e1acab8469b298
35844 <marjussi@uef.fi>
2024-05-05 13:03:29 +0300

    tietokantaan korjailua

--- OHTKOODIA/src/main/java/org/example/ohtkoodia/TietokannanYhdistaminen.java
+++ OHTKOODIA/src/main/java/org/example/ohtkoodia/TietokannanYhdistaminen.java
@@ -2,11 +2,13 @@ package org.example.ohtkoodia;
 import java.sql.*;
 public class TietokannanYhdistaminen {
     public static void main(String[] args) {
-        //Make: en ole varma onkohan localhost nyt 3306 vai 3307
-        String url = "jdbc:mysql://localhost:3306/vn";
+        //localhost:3307 on se, mihin porttiin teidän pitää liittää se XAMPP
+        //käyttis ja salasana on ne, joista make laittoi kuvakaappauksen discoon.
+        //tehkää sama käyttis niin tää toimii kaikilla
+        String url = "jdbc:mysql://localhost:3307/vn";
 
-        String username = "root";
-        String password = "";
+        String username = "pmauser";
+        String password = "password";
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
 
@@ -14,7 +16,7 @@ public class TietokannanYhdistaminen {
 
             Statement statement = connection.createStatement();
 
-            ResultSet resultSet = statement.executeQuery("select * from mokki");
+            ResultSet resultSet = statement.executeQuery("select * from alue");
 
             //datatyyppi + kolumni-indeksi haettavassa taulukossa
             while (resultSet.next()){
